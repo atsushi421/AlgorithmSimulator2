ee12c15e7f8a5df422d75f5b8dc92584f0f3a726
atushi421 <a.yano.578@ms.saitama-u.ac.jp>
2021-05-21 18:22:06 +0900

    class ClusteredManyCoreProcesser complete

--- class_Kalray.py
+++ class_Kalray.py
@@ -4,24 +4,84 @@
 class Core:
     # ＜コンストラクタ＞
     def __init__(self):
-        self.idle = True  # idle=True : このコアがアイドル状態
-        self.processing_node = -1  # 処理中のノード番号
-        self.remain_process = 0  # 残処理時間
+        '''
+        idle=True : このコアがアイドル状態
+        processing_node : 処理中のノード番号
+        remain_process : 残処理時間
+        finish_time : 処理終了時刻
+        '''
+        self.idle = True
+        self.processing_node = -1
+        self.remain_process = 0
+        self.finish_time = -1
     
+    
+    # ＜メソッド＞
+    # 処理を1秒進める
+    def advance_time(self):
+        if(self.idle == False):
+            self.remain_process-=1
+            if(self.remain_process == 0):
+                self.__init__()
+    
+
 
 class Cluster:
     # ＜コンストラクタ＞
     def __init__(self, num_of_core):
+        '''
+        num_of_core : 1クラスタ内のコア数
+        self.core[] : クラスタ内にあるコア
+        '''
+        
+        self.num_of_core = num_of_core
         # コアを用意
-        self.core = []  # このクラスタ内にあるコア
-        for i in range(num_of_core):
+        self.core = []
+        for i in range(self.num_of_core):
             self.core.append(Core())
+    
+    
+    # ＜メソッド＞
+    # 処理を1秒進める
+    def advance_time(self):
+        for i in range(self.num_of_core):
+            self.core[i].advance_time()
+    
+    # クラスタに空きがあればそのコア番号, そうでなければ-1
+    def idle_core(self):
+        for i in range(self.num_of_core):
+            if(self.core[i].idle == True):
+                return i
+        
+        return -1
             
 
+
 class ClusteredManyCoreProcesser:
+    # ＜クラス変数＞
+    current_time = 0  # 現在時刻
+    
+    
     # ＜コンストラクタ＞
     def __init__(self, num_of_cluster, num_of_core):
+        '''
+        num_of_cluster : プロセッサ内のクラスタ数
+        num_of_core : 1クラスタ内のコア数
+        cluster = [] : このプロセッサ内にあるクラスタ
+        '''
+        
+        self.num_of_cluster = num_of_cluster
+        self.num_of_core = num_of_core
         # プロセッサを形成
-        self.cluster = []  # このプロセッサ内にあるクラスタ
-        for i in range(num_of_cluster):
-            self.cluster.append(Cluster(num_of_core))
+        self.cluster = []
+        for i in range(self.num_of_cluster):
+            self.cluster.append(Cluster(self.num_of_core))
+    
+    
+    # ＜メソッド＞
+    # 処理を1進める
+    def advance_time(self):
+        ClusteredManyCoreProcesser.current_time+=1
+        
+        for i in range(self.num_of_cluster):
+            self.cluster[i].advance_time()
--- main.py
+++ main.py
@@ -8,4 +8,4 @@ from class_Kalray import ClusteredManyCoreProcesser
 dag1 = DAG("test_nine_task")
 
 kalray = ClusteredManyCoreProcesser(4,2)
-print(kalray.cluster[0].core[0].idle)
+print(kalray.cluster[0].idle_core())
