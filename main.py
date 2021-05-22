ba6a9674c41282597bf13df287ed266028809f37
atushi421 <a.yano.578@ms.saitama-u.ac.jp>
2021-05-22 22:41:48 +0900

    class_Q_learning fist

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 
 
 from class_DAG import DAG
-from class_Kalray import ClusteredManyCoreProcesser
+from class_DAG import one_entry_DAG
+from class_ClusteredManyCore import ClusteredManyCoreProcesser
 import HEFT
 from class_Scheduler import Scheduler
 
 
 dag1 = DAG("test_nine_task")
+dag1.print_all()
+dag2 = one_entry_DAG("test_nine_task")
+dag2.print_all()
 
-target = ClusteredManyCoreProcesser(1,2,3)
+target = ClusteredManyCoreProcesser(1,3,3)
 
-sl = HEFT.HEFT(dag1)
+sl = HEFT.HEFT(dag2)
+sl.remove(dag2.ve_index)
 
 scheduler = Scheduler(sl, dag1, target)
 scheduler.schedule()
